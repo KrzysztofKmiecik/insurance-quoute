4258503eafd8e2e0d61d40e0597e59c3fdad83a5
Joanna <Joanna@wp.pl>
2022-01-18 10:18:05 +0100

    empty tests creation

--- src/test/java/com/example/insurancequoute/repository/DriverDataRepositoryTest.java
+++ src/test/java/com/example/insurancequoute/repository/DriverDataRepositoryTest.java
@@ -1,7 +1,64 @@
 package com.example.insurancequoute.repository;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.fail;
 
 class DriverDataRepositoryTest {
+
+    @Test
+    public void shouldGetAllDrivers() {
+        fail();
+    }
+
+    @Test
+    public void shouldGetAllDriversNoRecords() {
+        fail();
+    }
+
+    @Test
+    public void shouldSaveNewDriver() {
+        fail();
+    }
+
+    @Test
+    public void shouldDeleteDriverById() {
+        fail();
+    }
+
+    @Test
+    public void shouldDeleteDriverByIdMissingDriver() {
+        fail();
+    }
+
+    @Test
+    public void shouldGetDriverById() {
+        fail();
+    }
+
+    @Test
+    public void shouldGetDriverByIdMissingDriver() {
+        fail();
+    }
+
+    @Test
+    public void shouldUpdatePhoneById() {
+        fail();
+    }
+
+    @Test
+    public void shouldUpdatePhoneByIdMissingDriver() {
+        fail();
+    }
+
+    @Test
+    public void shouldUpdateDriverById() {
+        fail();
+    }
+
+    @Test
+    public void shouldUpdateDriverByIdMissingDriver() {
+        fail();
+    }
 
 }
