36688ae6d1df52c8a805209b4dc614b783ff4c33
berejman <Beata.Rejman@ergo.de>
2022-01-18 11:31:28 +0100

    Service - scheme for tests with fail()

--- src/test/java/com/example/insurancequoute/service/DriverDataServiceTest.java
+++ src/test/java/com/example/insurancequoute/service/DriverDataServiceTest.java
@@ -1,7 +1,201 @@
 package com.example.insurancequoute.service;
 
+import com.example.insurancequoute.repository.DriverDataRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
 import static org.junit.jupiter.api.Assertions.*;
 
+
+@ExtendWith(MockitoExtension.class)
 class DriverDataServiceTest {
 
+    @Mock
+    private DriverDataRepository driverDataRepository;
+
+    @InjectMocks
+    private DriverDataService driverDataService;
+
+    @Test
+    void shouldReturnCorrectFactorValueWhenVehicleTypeCabriolet(){
+
+        fail();
+    }
+
+    @Test
+    void shouldReturnCorrectFactorValueWhenVehicleTypeCoupe(){
+
+        fail();
+    }
+
+    @Test
+    void shouldReturnCorrectFactorValueWhenVehicleTypeEstate(){
+
+        fail();
+    }
+
+    @Test
+    void shouldReturnCorrectFactorValueWhenVehicleTypeHatchback(){
+
+        fail();
+    }
+
+    @Test
+    void shouldReturnCorrectFactorValueWhenVehicleTypeOther(){
+
+        fail();
+    }
+
+    @Test
+    void shouldReturnCorrectFactorValueWhenEngineSize1000(){
+
+        fail();
+    }
+
+    @Test
+    void shouldReturnCorrectFactorValueWhenEngineSize1600(){
+
+        fail();
+    }
+
+    @Test
+    void shouldReturnCorrectFactorValueWhenEngineSize2000(){
+
+        fail();
+    }
+
+    @Test
+    void shouldReturnCorrectFactorValueWhenEngineSize2500(){
+
+        fail();
+    }
+
+    @Test
+    void shouldReturnCorrectFactorValueWhenEngineSize3000(){
+
+        fail();
+    }
+
+    @Test
+    void shouldReturnCorrectFactorValueWhenEngineSizeOther(){
+
+        fail();
+    }
+
+    @Test
+    void shouldReturnCorrectFactorValueWhenVehicleValueEqualOrGreater5000(){
+
+        fail();
+    }
+
+    @Test
+    void shouldReturnCorrectFactorValueWhenAdditionalDriversLessThan2(){
+
+        fail();
+    }
+
+    @Test
+    void shouldReturnCorrectFactorValueWhenAdditionalDriversEqualOrGreaterThan2(){
+
+        fail();
+    }
+
+    @Test
+    void shouldReturnCorrectFactorValueWhenCommercialUseTrue(){
+
+        fail();
+    }
+
+    @Test
+    void shouldReturnCorrectFactorValueWhenCommercialUseFalse(){
+
+        fail();
+    }
+
+    @Test
+    void shouldReturnCorrectFactorValueWhenOutsideStatusTrue(){
+
+        fail();
+    }
+
+    @Test
+    void shouldReturnCorrectFactorValueWhenOutsideStatusFalse(){
+
+        fail();
+    }
+
+    @Test
+    void testInsuranceQuotationCalculationWhenAllFactorsGiven(){
+
+        fail();
+    }
+
+    @Test
+    void testInsuranceQuotationCalculationWhenCabrioletGiven(){
+
+        fail();
+    }
+
+    @Test
+    void testInsuranceQuotationCalculationWhenNoVehicleTypeGiven(){
+
+        fail();
+    }
+
+    @Test
+    void testInsuranceQuotationCalculationWhenEngineSize2000Given(){
+
+        fail();
+    }
+
+    @Test
+    void testInsuranceQuotationCalculationWhenVehicleValue6500Given(){
+
+        fail();
+    }
+
+    @Test
+    void testInsuranceQuotationCalculationWhenAdditionalDrivers5Given(){
+
+        fail();
+    }
+
+    @Test
+    void testInsuranceQuotationCalculationWhenAdditionalDriversGiven(){
+
+        fail();
+    }
+
+    @Test
+    void testInsuranceQuotationCalculationWhenCommercialUseTrue(){
+
+        fail();
+    }
+
+    @Test
+    void testInsuranceQuotationCalculationWhenCommercialUseFalse(){
+
+        fail();
+    }
+
+    @Test
+    void testInsuranceQuotationCalculationWhenOutsideStateTrue(){
+
+        fail();
+    }
+
+    @Test
+    void testInsuranceQuotationCalculationWhenOutsideStateFalse(){
+
+        fail();
+    }
+
+    @Test
+    void testInsuranceQuotationCalculationWhenOutsideStateNotGiven(){
+
+        fail();
+    }
 }
