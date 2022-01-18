480f9868bfda4d7a49b375e28dcac6beeb90cf0a
Joanna <Joanna@wp.pl>
2022-01-18 14:46:03 +0100

    test getAllDrivers

--- src/test/java/com/example/insurancequoute/controller/DriverDataControllerTest.java
+++ src/test/java/com/example/insurancequoute/controller/DriverDataControllerTest.java
@@ -2,13 +2,17 @@ package com.example.insurancequoute.controller;
 
 import com.example.insurancequoute.service.DriverDataService;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import static org.junit.jupiter.api.Assertions.fail;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 
 @WebMvcTest
 class DriverDataControllerTest {
@@ -21,8 +25,12 @@ class DriverDataControllerTest {
     private DriverDataService driverDataService;
 
     @Test
-    public void getAllDrivers() {
-        fail();
+    @DisplayName("http://localhost/driver/getAll -> 200")
+    public void getAllDrivers() throws Exception {
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/driver/getAll"))
+                .andDo(print())
+                .andExpect(MockMvcResultMatchers.status().isOk());
     }
 
     @Test
