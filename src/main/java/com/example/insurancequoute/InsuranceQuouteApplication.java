62b71f53f2fed2947355ae4348bdd72abc8832fd
lukasze <wyspianski.lukasz@gmail.com>
2022-01-18 09:21:00 +0100

    Inicjalizacja projektu: Web, Jpa, H2, Lombok

---
+++ src/main/java/com/example/insurancequoute/InsuranceQuouteApplication.java
@@ -0,0 +1,13 @@
+package com.example.insurancequoute;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InsuranceQuouteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InsuranceQuouteApplication.class, args);
+    }
+
+}
